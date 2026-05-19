6caaaaaf3555036acc8649c89fdcf9b14410a07b
jhonny.silva <jhonnyms1012@gmail.com>
2026-05-18 23:27:12 -0300

    feat: Subistitui o bucket da alura para um bucket da minha AWS.

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-alura"
-    key    = "Prod/terraform.tfstate"
+    bucket = "tfstate-jhonny-monteiro"
+    key    = "Homolog/terraform.tfstate"
     region = "us-west-2"
   }
 }
