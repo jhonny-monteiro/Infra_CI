db71f7ae53ab67139f7cc2e94f5297203861fd50
jhonny.silva <jhonnyms1012@gmail.com>
2026-05-18 22:45:00 -0300

    fix: Corrige problema de versionamento que acontece ao utilizar o comandos em versões que estão mais avançadas e não funcionam.

--- infra/ECS.tf
+++ infra/ECS.tf
@@ -1,5 +1,6 @@
 module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "~> 5.11"
 
   cluster_name       = var.ambiante
   fargate_capacity_providers = {
--- infra/VPC.tf
+++ infra/VPC.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
 
   name = "VPC-ECS"
   cidr = "10.0.0.0/16" #10.0.1.1 - 10.0.255.255
